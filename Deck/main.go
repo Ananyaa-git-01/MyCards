ed5f8e2831ad276ee827a0bc4f9e4d70244845ea
anaawast <ananya.a.awasthi@capgemini.com>
2025-05-27 17:14:18 +0530

    shuffle function with random number generation

--- Deck/main.go
+++ Deck/main.go
@@ -3,10 +3,12 @@ package main
 func main() {
 	//cards := []string{"hello , ", newCards()}
 	//cards := deck{"hello , ", newCards()} //this is the same as the line above because in the same package we did type deck []string
-	// cards := newDeck() //clubs of ace
-	cards := ReadtheFile("My_car") // to read the file
+	cards := newDeck() //clubs of ace
+	cards.shuffle()
 	cards.print()
+	//cards := ReadtheFile("My_cards") // to read the file
 	//cards = append(cards, "six of spade ")
+
 	//fmt.Println("Cards in the main func %+v", cards)
 	//fmt.Println(cards.toString()) //cards ko one by one print krega
 	//dot means ... cards ko "print" kro
